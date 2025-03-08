e703b982f5b633714ccd668ba86ae3027d02278e
eviefowler <evie.fowler@gmail.com>
2025-03-08 10:35:34 -0500

    add DMC floss conversion

--- patternutils.py
+++ patternutils.py
@@ -3,6 +3,8 @@ import numpy as np
 from collections import Counter
 from matplotlib import pyplot as plt
 from pandas import read_csv
+from matplotlib.patches import Patch
+from sklearn.neighbors import KNeighborsClassifier
 
 def read_image(image_path):
     """ Load a saved image from the specified path """
@@ -28,6 +30,27 @@ def show_image(image, as_pattern = False, save_path = False):
         ax.grid(which = 'major', linewidth = 1, color = 'black')
         ax.grid(which = 'minor', linewidth = 0.5)
         ax.minorticks_on()
+        
+        # and add a legend
+        colorCounts = count_colors(image)
+        legendElements = []
+        for clr in colorCounts:
+            try:
+                dmc = flossWalk.loc[clr, 'DMC Number']
+                desc = flossWalk.loc[clr, 'Description']
+            except:
+                dmc = 0
+                desc = 'not a DMC color'
+            scld = [x / 255 for x in pixstl(clr)]
+            lbl = f'{dmc} ({desc}); Stitch Count: {colorCounts[clr]}'
+            
+            legendElements.append(Patch(facecolor = scld, label = lbl))
+            
+        plt.legend(handles = legendElements
+                   , loc = 'upper center'
+                   , bbox_to_anchor = (0.5, -0.15)
+                  )
+
     else:
         ax.axis('off')
     
@@ -111,6 +134,27 @@ def reduce_colors(image, color_target = 10, attempts = 1):
     newImage = newImageFlat.reshape((image.shape))
     
     return(newImage)
+
+def convert_to_dmc(image):
+    # use a single nearest neighbor model to map any new color to the most similar color with a known DMC floss match
+    X = np.array(flossWalk[['Red', 'Green', 'Blue']])
+    y = np.array(flossWalk.index)
+    neighborColor = KNeighborsClassifier(n_neighbors = 1).fit(X, y)
+
+    # identify all colors in the target image
+    colorCounts = count_colors(image)
+    
+    # map each color to its nearest DMC match
+    colorMap = {}
+    for clr in colorCounts:
+        colorMap[clr] = neighborColor.predict(np.array([pixstl(clr)]))[0]
+    
+    # replace colors, pixel by pixel
+    for row in range(image.shape[0]):
+        for col in range(image.shape[1]):
+            image[row, col, :] = np.array(pixstl(colorMap[pixlts(image[row, col, :])]))
+    
+    return(image)
 
 # load RGB-DMC floss number crosswalk
 flossWalk = read_csv('dmcRGB.txt', delimiter = ',')
